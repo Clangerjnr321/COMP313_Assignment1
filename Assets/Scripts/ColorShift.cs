e55eb69cddb325871cd352f97c7716a7725d4b0d
Cameron Laing <clangerjnr@gmail.com>
2019-08-12 14:02:40 +1200

    Added files from Josh as we had big merge conflicts

---
+++ Assets/Scripts/ColorShift.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorShift : MonoBehaviour
+{
+    public Renderer rend;
+    public Transform tran;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        tran = GetComponent<Transform>();
+    }
+
+    public void ColorChange()
+    {
+        rend.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+
+    public void OnCollisionEnter (Collision collisionInfo)
+    {
+      if (collisionInfo.collider.tag == "Obstacle")
+      {
+        ColorChange();
+        Debug.Log("BIG new colour time B)");
+      }
+    }
+
+    //
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     ColorChange();
+    // }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    Transform tr;
+    public ColorShift cube;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+}
